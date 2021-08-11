6cfbeb24eef815943a6084c61147456263000fc1
Dmitry Efremov <59552138+defremov-cscart@users.noreply.github.com>
2021-08-11 14:53:02 +0400

    Update centos7.sh

--- OS/centos7.sh
+++ OS/centos7.sh
@@ -25,6 +25,10 @@ echo '{
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
+systemctl start docker
+systemctl enable docker
+usermod -aG docker $USER
+
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
