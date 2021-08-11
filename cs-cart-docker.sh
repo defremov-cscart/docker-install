642acd037843616762dea5b24a24962c38b67d37
Dmitry Efremov <59552138+defremov-cscart@users.noreply.github.com>
2021-08-11 15:59:15 +0400

    Create cs-cart-docker.sh

---
+++ cs-cart-docker.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+### Check OS version
+set -e
+ if cat /etc/*release | grep ^PRETTY_NAME | grep "Ubuntu 16.04"; then
+    printf '\nInstalling on Ubuntu 16.04\n\n'
+
+apt-get -qq update -y
+apt -qq install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/ubuntu16.sh" -o ubuntu16.sh
+sh ubuntu16.sh
+
+elif cat /etc/*release | grep ^PRETTY_NAME | grep "Ubuntu 18.04"; then
+    printf '\nInstalling on Ubuntu 18.04\n\n' 
+
+apt-get -qq update -y
+apt -qq install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/ubuntu18.sh" -o ubuntu18.sh
+sh ubuntu18.sh
+
+elif cat /etc/*release | grep ^PRETTY_NAME | grep "Ubuntu 20.04"; then
+    printf '\nInstalling on Ubuntu 20.04\n\n' 
+
+apt-get -qq update -y
+apt -qq install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/ubuntu20.sh" -o ubuntu20.sh
+sh ubuntu20.sh
+
+elif cat /etc/*release | grep ^PRETTY_NAME | grep "CentOS Linux 7"; then
+    printf '\nInstalling on CentOS Linux 7\n\n' 
+
+### Installing packages
+printf '\nInstalling packages...\n\n'
+yum install -q -y yum-utils \
+    device-mapper-persistent-data \
+    lvm2 \
+    deltarpm
+
+curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/centos7.sh" -o centos7.sh
+sh centos7.sh
+
+elif cat /etc/*release | grep ^PRETTY_NAME | grep "CentOS Linux 8"; then
+    printf '\nInstalling on CentOS Linux 8\n\n' 
+
+### Installing packages
+printf '\nInstalling packages...\n\n'
+yum install -q -y yum-utils \
+    device-mapper-persistent-data \
+    lvm2
+
+curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/centos8.sh" -o centos8.sh
+sh centos8.sh
+
+elif cat /etc/*release | grep ^PRETTY_NAME | grep "Debian GNU/Linux 10"; then
+    printf '\nInstalling on Debian GNU/Linux 10\n\n' 
+
+apt-get -qq update -y
+apt -qq install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/debian10.sh" -o debian10.sh
+sh debian10.sh
+
+elif cat /etc/*release | grep ^PRETTY_NAME | grep "Debian GNU/Linux 11"; then
+    printf '\nInstalling on Debian GNU/Linux 11\n\n' 
+
+apt-get -qq update -y
+apt -qq install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/debian11.sh" -o debian11.sh
+sh debian11.sh
+
+else
+   printf '\nWRONG OS VERSION\n\n'
+   exit 1;
+fi
+
+exit 0
