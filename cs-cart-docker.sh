d54008209fcdf4063c7e975f292aa27ad3a2f3fc
Dmitry Efremov <59552138+defremov-cscart@users.noreply.github.com>
2021-08-11 17:05:16 +0400

    Update cs-cart-docker.sh

--- cs-cart-docker.sh
+++ cs-cart-docker.sh
@@ -4,13 +4,13 @@ set -e
  if cat /etc/*release | grep ^PRETTY_NAME | grep "Ubuntu 16.04"; then
     printf '\nInstalling on Ubuntu 16.04\n\n'
 
-apt-get -qq update -y
+apt-get -qq update -y > /dev/null
 apt -qq install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common  > /dev/null
 
 curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/ubuntu16.sh" -o ubuntu16.sh
 sh ubuntu16.sh
@@ -18,13 +18,13 @@ sh ubuntu16.sh
 elif cat /etc/*release | grep ^PRETTY_NAME | grep "Ubuntu 18.04"; then
     printf '\nInstalling on Ubuntu 18.04\n\n' 
 
-apt-get -qq update -y
+apt-get -qq update -y > /dev/null
 apt -qq install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common  > /dev/null
 
 curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/ubuntu18.sh" -o ubuntu18.sh
 sh ubuntu18.sh
@@ -32,13 +32,13 @@ sh ubuntu18.sh
 elif cat /etc/*release | grep ^PRETTY_NAME | grep "Ubuntu 20.04"; then
     printf '\nInstalling on Ubuntu 20.04\n\n' 
 
-apt-get -qq update -y
+apt-get -qq update -y > /dev/null
 apt -qq install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common  > /dev/null
 
 curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/ubuntu20.sh" -o ubuntu20.sh
 sh ubuntu20.sh
@@ -51,7 +51,7 @@ printf '\nInstalling packages...\n\n'
 yum install -q -y yum-utils \
     device-mapper-persistent-data \
     lvm2 \
-    deltarpm
+    deltarpm  > /dev/null
 
 curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/centos7.sh" -o centos7.sh
 sh centos7.sh
@@ -63,7 +63,7 @@ elif cat /etc/*release | grep ^PRETTY_NAME | grep "CentOS Linux 8"; then
 printf '\nInstalling packages...\n\n'
 yum install -q -y yum-utils \
     device-mapper-persistent-data \
-    lvm2
+    lvm2  > /dev/null
 
 curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/centos8.sh" -o centos8.sh
 sh centos8.sh
@@ -71,13 +71,13 @@ sh centos8.sh
 elif cat /etc/*release | grep ^PRETTY_NAME | grep "Debian GNU/Linux 10"; then
     printf '\nInstalling on Debian GNU/Linux 10\n\n' 
 
-apt-get -qq update -y
+apt-get -qq update -y  > /dev/null
 apt -qq install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common  > /dev/null
 
 curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/debian10.sh" -o debian10.sh
 sh debian10.sh
@@ -85,13 +85,13 @@ sh debian10.sh
 elif cat /etc/*release | grep ^PRETTY_NAME | grep "Debian GNU/Linux 11"; then
     printf '\nInstalling on Debian GNU/Linux 11\n\n' 
 
-apt-get -qq update -y
+apt-get -qq update -y  > /dev/null
 apt -qq install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common  > /dev/null
 
 curl -fsSL "https://raw.githubusercontent.com/defremov-cscart/docker-install/main/OS/debian11.sh" -o debian11.sh
 sh debian11.sh
