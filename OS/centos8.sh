697d9030f334c84b0debb975fac010f2b093fdfe
Dmitry Efremov <59552138+defremov-cscart@users.noreply.github.com>
2021-08-11 14:55:06 +0400

    Create centos8.sh

---
+++ OS/centos8.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+printf '\nInstalling on CentOS Linux 8\n\n'
+
+### Generate docker config
+printf '\nPrepare config for overlay2...\n\n'
+mkdir /etc/docker
+touch /etc/docker/daemon.json
+
+echo '{
+    "dns": ["1.1.1.1"],
+    "default-address-pools": [{
+		  "base": "172.100.0.0/8",
+		  "size": 16
+	  }],
+    "live-restore": true,
+	  "storage-driver": "overlay2",
+	  "storage-opts": [
+ 	   "overlay2.override_kernel_check=true"
+	    ]
+	  }' > /etc/docker/daemon.json
+
+### Docker install
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+systemctl start docker
+systemctl enable docker
+usermod -aG docker $USER
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+printf '\n====YOU NEED TO REBOOT SERVER====\n\n'
+
+exit 0
